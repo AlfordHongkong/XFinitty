8280da81f563bdcff3d3ed7bf2cb807fac8e7923
Rock Deng <dengyongpeng110@outlook.com>
2018-11-15 10:26:27 +0800

    completed circular buffer

--- XFinitty/app/circular_buffer.c
+++ XFinitty/app/circular_buffer.c
@@ -1,5 +1,48 @@
-
+/**
+ * @file circular_buffer.c
+ * @author Rock Deng
+ * @brief 
+ * @version 0.1
+ * @date 2018-11-15
+ * 
+ * @copyright Copyright (c) 2018
+ * 
+ */
 
 
 #include "ringBuffer.h"
 
+
+
+uint8_t InitCircularBuf(circularBuffer_t *cb, uint8_t *buf){
+    /// assert two arguments
+    uint8_t size;
+    size = sizeof(buf);
+    if ( (!size || (size & (size - 1))) != 0) { return 1;} ///< size is not a power of tow
+
+    cb->buf = buf;
+    cb->size = size;
+    cb->read = 0;
+    cb->write = 0;
+
+    return 0;
+}
+
+uint8_t GetLengthOfCircularBuf(circularBuffer_t *cb){
+    return ((cb->write - cb->read) & (cb->size -1));
+}
+
+circular_buf_error_t ReadCircularBuf(circularBuffer_t *cb, uint8_t data){
+    if (GetLengthOfCircularBuf(cb) == (cb->size-1)) { return error_buffer_full}
+    cb->buf[cb-write] = data;
+    cb->write = (cb-write + 1) & (cb->size -1); // must be atomic
+
+    return no_error;
+}
+circular_buf_error_t WriteCircularBuf(circularBuffer_t *cb, uint8_t *data){
+    if (GetLengthOfCircularBuf(cb) == 0) { return error_buffer_empty;}
+    *data = cb->buf[cb->read];
+    cb->read = (cb-read +1) & (cb->size - 1);
+
+    return no_error;
+}
--- XFinitty/app/circular_buffer.h
+++ XFinitty/app/circular_buffer.h
@@ -1,4 +1,13 @@
-
+/**
+ * @file circular_buffer.h
+ * @author Rock Deng
+ * @brief 
+ * @version 0.1
+ * @date 2018-11-15
+ * 
+ * @copyright Copyright (c) 2018
+ * 
+ */
 
 
 #ifndef __RINGBUFFER_H__
@@ -6,18 +15,62 @@
 
 #include "stdint.h"
 
+/**
+ * @brief buffer structure
+ * 
+ */
 typedef struct{
-    uint8_t *buf;
-    uint8_t size;
-    uint8_t read;
-    uint8_t write;
+    uint8_t *buf; ///< block of memory
+    uint8_t size; ///< must be a power of two
+    uint8_t read; ///< holds current read position: 0 to (size-1)
+    uint8_t write; ///< holds current write position: 0 to (size-1)
 }circularBuffer_t;
 
+/**
+ * @brief error flag 
+ * 
+ */
+typedef enum{
+    no_error,
+    error_buffer_full,
+    error_buffer_empty,
 
-uint8_t InitCircularBuffer();
-uint8_t GetLengthOfCircularBuf();
-uint8_t ReadCircularBuf();
-uint8_t WriteCircularBuf();
+}circular_buf_error_t;
+
+/**
+ * @brief Initialize a circular buffer structer
+ * 
+ * @param cb - pointer of the circular buffer structer
+ * @param buf - pointer of the buffer
+ * @return uint8_t - 1 means error, 0 means success
+ */
+uint8_t InitCircularBuf(circularBuffer_t *cb, uint8_t *buf);
+
+/**
+ * @brief Get the Length Of Circular Buf object
+ * 
+ * @param cb - pointer of the circular buffer
+ * @return uint8_t - 1 means error, 0 means success
+ */
+uint8_t GetLengthOfCircularBuf(circularBuffer_t *cb);
+
+/**
+ * @brief get a item for the circular buffer
+ * 
+ * @param cb - pointer of the circular buff
+ * @param data - the data return from the buffer
+ * @return circular_buf_error_t - state of the processing
+ */
+circular_buf_error_t ReadCircularBuf(circularBuffer_t *cb, uint8_t *data);
+
+/**
+ * @brief write a data into the buffer
+ * 
+ * @param cb - pointer of the buffer structure
+ * @param data - the data to be write
+ * @return circular_buf_error_t - state of the processing
+ */
+circular_buf_error_t WriteCircularBuf(circularBuffer_t *cb, uint8_t data);
 
 
 #endif
